285d54326b1b294017daad2fdd4ee96e5514ebec
btcpt123 <115025678+btcpt123@users.noreply.github.com>
2023-01-03 22:06:29 +0000

    locs

--- restorefiles.sh
+++ restorefiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://cdn.jsdelivr.net/npm/docsify/lib/themes/buble.css > static/assets/css/buble.css
-curl https://cdn.jsdelivr.net/npm/docsify@4 > static/assets/js/docsify.js
-curl https://cdn.jsdelivr.net/npm/docsify/lib/plugins/search.min.js > static/assets/js/search.min.js
+curl https://cdn.jsdelivr.net/npm/docsify/lib/themes/buble.css > static/css/buble.css
+curl https://cdn.jsdelivr.net/npm/docsify@4 > static/js/docsify.js
+curl https://cdn.jsdelivr.net/npm/docsify/lib/plugins/search.min.js > static/js/search.min.js
